0a93bf451844f60f19de6bc1e7d69a47cdb2752f
Alexey Ermakov <strangebrew@yandex.ru>
2023-05-01 14:02:33 +0300

    L4 - template basics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
 import { ref } from 'vue';
+import socksGreenImage from './assets/images/socks_green.jpeg';
 
 const product = ref('Socks');
+const image = ref(socksGreenImage);
+const inStock = ref(true);
+
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+
+const variants = ref([
+  { id: 2234, color: 'green' },
+  { id: 2235, color: 'blue' },
+])
 </script>
 
 <template>
-  <h1>{{ product }}</h1>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+      </div>
+    </div>
+  </div>
 </template>
