08dc2d116e16bc589e03d594b334bce04f4b33e3
Alexey Ermakov <strangebrew@yandex.ru>
2023-05-01 14:17:57 +0300

    L5 - event handling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import socksGreenImage from './assets/images/socks_green.jpeg';
+import socksBlueImage from './assets/images/socks_blue.jpeg';
 
 const product = ref('Socks');
 const image = ref(socksGreenImage);
@@ -9,13 +10,22 @@ const inStock = ref(true);
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
 ])
+
+const cart = ref(0);
+
+const addToCart = () => cart.value += 1;
+
+const updateImage = (variantImage) => {
+  image.value = variantImage;
+}
 </script>
 
 <template>
   <div class="nav-bar"></div>
+  <div class="cart">Cart ({{ cart }})</div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -28,7 +38,14 @@ const variants = ref([
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+        <div
+          v-for="variant in variants"
+          :key="variant.id"
+          @mouseover="updateImage(variant.image)"
+        >
+          {{ variant.color }}
+        </div>
+        <button class="button" @click="addToCart">Add to cart</button>
       </div>
     </div>
   </div>
